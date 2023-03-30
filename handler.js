02b0e862da6322827936ef7ac7fc17366ef95a9f
yu <mail@firdaus.org>
2023-03-31 04:16:24 +0700

    Add deposit handler

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const service = require('./service');
+const { isNumber } = require('./util');
 
 const login = async (accountName) => {
   try {
@@ -18,7 +19,16 @@ const logout = async () => {
 };
 
 const deposit = async (amount) => {
-  console.log(amount);
+  try {
+    if (!isNumber(amount)) {
+      throw new Error('The amount is not a number');
+    }
+
+    const balance = await service.deposit(Number(amount));
+    console.log(`Your balance is $${balance}`);
+  } catch (e) {
+    console.error(`Error: ${e.message}`);
+  }
 };
 
 const transfer = async (destination, amount) => {
