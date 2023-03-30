e04b8ba0bf8ca5aa2ace3b459af8a94f4e8efca9
yu <mail@firdaus.org>
2023-03-31 01:57:52 +0700

    Add session library

---
+++ session.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const sessionDatabase = 'session.json';
+
+const loadSession = async () => {
+  const dataBuffer = await fs.promises.readFile(sessionDatabase);
+  return dataBuffer.toString();
+};
+
+const clearSession = async () => {
+  await fs.promises.writeFile(sessionDatabase, '');
+};
+
+const saveSession = async (accountName) => {
+    const dataBuffer = await fs.promises.readFile(sessionDatabase);
+    const currentSession = dataBuffer.toString();
+    
+    if (currentSession === "") {
+      await fs.promises.writeFile(sessionDatabase, accountName);
+    } else {
+      throw new Error(`Account ${currentSession} must logout first`);
+    }
+  };
+
+module.exports = {
+  clearSession,
+  loadSession,
+  saveSession,
+};
