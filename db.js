82f9015bde0a465a7c07da2a74a7ca68e546cefa
yu <mail@firdaus.org>
2023-03-31 17:32:18 +0700

    Inject account database

--- db.js
+++ db.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
 
-const accountDatabase = 'accounts.json';
+// the account database
+let _accountDatabase;
 
+/**
+ * Initialize account database.
+ */
+const initDatabase = async (db) => {
+  _accountDatabase = db;
+};
+
+/**
+ * Load accounts.
+ * @throws an error if the database is not initialized.
+ * @returns {object} - All the accounts in a key-value object. On error, return {}
+ */
 const loadAccounts = async () => {
+  if (!_accountDatabase) {
+    throw new Error('Database has not been initialized.');
+  }
+
   try {
-    const dataBuffer = await fs.promises.readFile(accountDatabase);
+    const dataBuffer = await fs.promises.readFile(_accountDatabase);
     const dataJSON = dataBuffer.toString();
     return JSON.parse(dataJSON);
   } catch (e) {
@@ -12,12 +29,21 @@ const loadAccounts = async () => {
   }
 };
 
+/**
+ * Save accounts.
+ * @throws an error if the database is not initialized.
+ */
 const saveAccounts = async (data) => {
+  if (!_accountDatabase) {
+    throw new Error('Database has not been initialized.');
+  }
+
   const dataJSON = JSON.stringify(data);
-  await fs.promises.writeFile(accountDatabase, dataJSON);
+  await fs.promises.writeFile(_accountDatabase, dataJSON);
 };
 
 module.exports = {
+  initDatabase,
   loadAccounts,
   saveAccounts,
 };
