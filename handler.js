c64d68fcbb1c825c10ad21669eeb6d2240d7e5ad
yu <mail@firdaus.org>
2023-04-01 15:07:49 +0700

    Add moneyFormatter

--- handler.js
+++ handler.js
@@ -1,10 +1,10 @@
 const service = require('./service');
-const { isNumber } = require('./util');
+const { isNumber, moneyFormatter } = require('./util');
 
 const login = async (accountName) => {
   try {
     const account = await service.login(accountName);
-    console.log(`Hello ${account.name}. Your balance is ${account.balance}.`);
+    console.log(`Hello ${account.name}. Your balance is ${moneyFormatter(account.balance)}.`);
   } catch (e) {
     console.error(`Error: ${e.message}`);
   }
@@ -21,7 +21,7 @@ const logout = async () => {
 const deposit = async (amount) => {
   try {
     const balance = await service.deposit(amount);
-    console.log(`Your balance is $${balance}`);
+    console.log(`Your balance is $${moneyFormatter(balance)}`);
   } catch (e) {
     console.error(`Error: ${e.message}`);
   }
@@ -31,7 +31,7 @@ const transfer = async (destination, amount) => {
   try {
     const balance = await service.transfer(destination, amount);
     console.log(
-      `Transferred $${amount} to ${destination}. Your balance is $${balance}`,
+      `Transferred $${amount} to ${destination}. Your balance is $${moneyFormatter(balance)}`,
     );
   } catch (e) {
     console.error(`Error: ${e.message}`);
@@ -41,7 +41,7 @@ const transfer = async (destination, amount) => {
 const withdraw = async (amount) => {
   try {
     const balance = await service.withdraw(amount);
-    console.log(`You withdraw $${amount}. Your balance is $${balance}`);
+    console.log(`You withdraw $${amount}. Your balance is $${moneyFormatter(balance)}`);
   } catch (e) {
     console.error(`Error: ${e.message}`);
   }
