aa4434997d88ca2714f79108ec04134cd3e302f3
yu <mail@firdaus.org>
2023-04-01 14:51:58 +0700

    Add moneyFormatter

--- util.js
+++ util.js
@@ -3,13 +3,27 @@
  * @param {any} x - The input.
  * @returns {bool} true if it's a number, else false.
  */
-function isNumber(x) {
+const isNumber = (x) => {
   if (typeof x === 'number' && !Number.isNaN(x)) {
     return true;
   }
   return false;
-}
+};
+
+/**
+ * Format dollar amount.
+ * @param {any} x - The input.
+ * @returns {string} The formatted amount.
+ */
+const moneyFormatter = (x) => {
+  if (typeof x === 'number' && !Number.isNaN(x)) {
+    const isNegative = x < 0 ? '-' : '';
+    return `${isNegative}$${Math.abs(x)}`;
+  }
+  throw new Error('amount must be number');
+};
 
 module.exports = {
   isNumber,
+  moneyFormatter,
 };
