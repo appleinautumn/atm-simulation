62c838e7e86ac27e8b12fc498856655af98c4ca2
yu <mail@firdaus.org>
2023-04-18 01:56:38 +0700

    Add loadLoanAccounts, saveLoans functions

--- db.js
+++ db.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 
 // the account database
 let _accountDatabase;
+let _loanDatabase;
 
 /**
  * Initialize account database.
  */
-const initDatabase = async (db) => {
+const initDatabase = async (db, loanDb) => {
   _accountDatabase = db;
+  _loanDatabase = loanDb;
 };
 
 /**
@@ -30,6 +32,25 @@ const loadAccounts = async () => {
 };
 
 /**
+ * Load loan accounts.
+ * @throws an error if the database is not initialized.
+ * @returns {object} - All the accounts in a key-value object. On error, return {}
+ */
+const loadLoanAccounts = async () => {
+  if (!_loanDatabase) {
+    throw new Error('Database has not been initialized.');
+  }
+
+  try {
+    const dataBuffer = await fs.promises.readFile(_loanDatabase);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return {};
+  }
+};
+
+/**
  * Save accounts.
  * @throws an error if the database is not initialized.
  */
@@ -42,8 +63,23 @@ const saveAccounts = async (data) => {
   await fs.promises.writeFile(_accountDatabase, dataJSON);
 };
 
+/**
+ * Save loan accounts.
+ * @throws an error if the database is not initialized.
+ */
+const saveLoans = async (data) => {
+  if (!_loanDatabase) {
+    throw new Error('Database has not been initialized.');
+  }
+
+  const dataJSON = JSON.stringify(data);
+  await fs.promises.writeFile(_loanDatabase, dataJSON);
+};
+
 module.exports = {
   initDatabase,
   loadAccounts,
+  loadLoanAccounts,
   saveAccounts,
+  saveLoans,
 };
